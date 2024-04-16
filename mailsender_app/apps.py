48e24cbe227d457496d3c02ff64bfa8916cf17df
egor <ega.nik5674@gmail.com>
2024-04-16 17:28:25 +0300

    Update apps.py

--- mailsender_app/apps.py
+++ mailsender_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MailsenderAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mailsender_app'
+    verbose_name = 'Рассылка'
