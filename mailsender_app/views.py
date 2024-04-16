feb45184d8c2b1d5c906aaafc74055744178ed4d
egor <ega.nik5674@gmail.com>
2024-04-16 17:50:18 +0300

    Update views.py

--- mailsender_app/views.py
+++ mailsender_app/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView, DetailView, UpdateView
 
-# Create your views here.
+from mailsender_app.forms import ClientForm
+from mailsender_app.models import Client
+
+
+class Index(TemplateView):
+    template_name = 'mailsender/index.html'
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    form_class = ClientForm
+    success_url = reverse_lazy('mailsender:main')
+
+
+class ClientDetailView(DetailView):
+    model = Client
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
