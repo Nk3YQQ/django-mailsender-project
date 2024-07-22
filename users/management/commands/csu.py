5f4f84dcfc0c38d043c2b99c66f3f495d9353480
egor <ega.nik5674@gmail.com>
2024-07-22 12:18:56 +0300

    Update csu.py

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -1,17 +1,23 @@
+from django.contrib.auth.hashers import make_password
 from django.core.management import BaseCommand
+from django.db import IntegrityError
 
 from users.models import User
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        superuser = User.objects.create(
-            email='admin@mail.ru',
-            first_name='Admin',
-            last_name='Adminov',
-            is_staff=True,
-            is_superuser=True
-        )
+        try:
+            superuser, created = User.objects.get_or_create(
+                email='admin@mail.ru',
+                first_name='Admin',
+                last_name='Adminov',
+                password=make_password('908poi543tre'),
+                is_staff=True,
+                is_superuser=True
+            )
+            if created:
+                self.stdout.write(self.style.SUCCESS('User "Admin" was created'))
 
-        superuser.set_password('908poi543tre')
-        superuser.save()
+        except IntegrityError:
+            self.stdout.write('User "Admin" already exists')
